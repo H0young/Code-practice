5e04a8c5341ba40a33f179fa178e4db7406de930
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-19 19:10:20 +0900

    프로그래머스-코딩테스트 연습-월간 코드 챌린지: 두 개 뽑아서 더하기

---
+++ JavaScript/For003.js
@@ -0,0 +1,17 @@
+// 두 개 뽑아서 더하기 (2020.09.19.)
+// https://programmers.co.kr/learn/courses/30/lessons/68644
+
+function solution(numbers) {
+  var answer = [];
+
+  for (var i = 0; i < numbers.length; i++) {
+    for (var j = 0; j < numbers.length; j++) {
+      if (i !== j) {
+        answer.push(numbers[i] + numbers[j]);
+        var answerSet = Array.from([...new Set(answer)]);
+        var answerSort = answerSet.sort((a, b) => a - b);
+      }
+    }
+  }
+  return answerSort;
+}
