f1d15a7ddb5027ba6a9512b84370f0f640f4051d
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-21 23:39:23 +0900

    For003 answerSet 배열화 함수 삭제, 1줄 답 추가

--- JavaScript/For003.js
+++ JavaScript/For003.js
@@ -8,8 +8,9 @@ function solution(numbers) {
     for (var j = 0; j < numbers.length; j++) {
       if (i !== j) {
         answer.push(numbers[i] + numbers[j]);
-        var answerSet = Array.from([...new Set(answer)]);
+        var answerSet = [...new Set(answer)];
         var answerSort = answerSet.sort((a, b) => a - b);
+        // answer = [...new Set(answer)].sort((a,b)=>a-b)
       }
     }
   }
